b4bb7d6dff99b4df45638f1dc7c30f384eb16bf4
leeseojune <sung07288346@gmail.com>
2021-06-17 16:47:10 +0900

    📑 :: ParseException

---
+++ lib/src/main/java/com/leeseojune/neisapi/exceptions/detailed/ParseException.java
@@ -0,0 +1,11 @@
+package com.leeseojune.neisapi.exceptions.detailed;
+
+import com.leeseojune.neisapi.exceptions.NeisException;
+
+public class ParseException extends NeisException {
+
+    public ParseException() {
+        super();
+    }
+
+}
