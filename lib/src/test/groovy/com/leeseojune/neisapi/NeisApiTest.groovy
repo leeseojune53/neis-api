2849fe06517418fa93fd6144fde5fa0a736e227e
leeseojune <sung07288346@gmail.com>
2021-06-22 21:09:31 +0900

    📑 :: NeisApiTest

---
+++ lib/src/test/groovy/com/leeseojune/neisapi/NeisApiTest.groovy
@@ -0,0 +1,14 @@
+package com.leeseojune.neisapi
+
+import spock.lang.Specification
+
+class NeisApiTest extends Specification {
+    def "GetSchoolByName"() {
+    }
+
+    def "GetMealsByAbsoluteDay"() {
+    }
+
+    def "GetMealsByRelativeDay"() {
+    }
+}
